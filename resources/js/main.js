92816e56ca501f0d1b26d258f68bacfbfbf01479
Paolo Rotolo <paolorotolo@ubuntu.com>
2014-11-25 22:44:21 +0100

    Fixed bug on Firefox.

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,5 +1,14 @@
+
+$(document).ready(function(){  
+
 function toggleDialog(transition) {
       var dialog = document.
         querySelector('paper-dialog[transition=' + transition + ']');
       dialog.toggle();
     }
+var button = document.getElementById('download');
+
+button.addEventListener('click', function(event) {
+toggleDialog('core-transition-center')
+});
+});
